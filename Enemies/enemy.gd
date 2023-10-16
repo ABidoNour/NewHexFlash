819ce4863e1ef19060e829d627507e82ddc636cf
kladak <104019263+kladak@users.noreply.github.com>
2023-10-15 20:50:13 -0500

    trying to add enemy health
    currently getting this error:
    
    E 0:00:08:0240   wand.gd:37 @ _on_body_entered(): Error calling from signal 'hit' to callable: 'Area2D(wand.gd)::_on_hit': Method not found.
      <C++ Source>   core/object/object.cpp:1082 @ emit_signalp()
      <Stack Trace>  wand.gd:37 @ _on_body_entered()

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -4,9 +4,27 @@ var direction = Vector2.ZERO  # Declare and initialize 'direction'
 @export var speed = 400  # Speed of the enemy
 var player = null  # Declare a variable to hold the player node
 var chase_player = false
+@export var health : int = 3
 
+
+func _on_area2D_body_entered(body):
+	if body.is_in_group("projectiles"):
+#		body.connect("hit", self, "_on_Wand_hit")
+		health -= 1
+		print(health)
+		if health <= 0:
+			queue_free()
+
+			
 func _ready():
 	$AnimatedSprite2D.play("default")
+	add_to_group('mob')
+#	get_tree().connect("wand_hit", self, "_on_Wand_hit")
+
+#func _on_Wand_hit(body):
+#	if body == self:
+#		body.disconnect("hit", self, "_on_Wand_hit")
+
 
 func _physics_process(delta):
 	if chase_player:
@@ -14,13 +32,14 @@ func _physics_process(delta):
 		velocity = direction * speed
 		move_and_slide()
 
-func _on_area2D_body_entered(body):
-	if body.is_in_group("projectiles"):
-		body.queue_free()
-		# Connect the 'hit' signal and then emit it
-		body.connect("hit", self, "_on_Wand_hit")
-		body.emit_signal("hit")
-		queue_free()
+#func _on_area2D_body_entered(body):
+#	if body.is_in_group("projectiles"):
+#		health -= 1
+#		print(health)
+#		# Connect the 'hit' signal and then emit it
+##		body.connect("hit", self, "_on_Wand_hit")
+##		body.emit_signal("hit")
+##		queue_free()
 
 func _on_detection_area_body_entered(body):
 	player = body
@@ -29,3 +48,10 @@ func _on_detection_area_body_entered(body):
 
 func _on_detection_area_body_exited(body):
 	chase_player = false
+	
+func _on_hit(body):
+	if body == self:
+		health -= 1
+		print(health)
+		if health <= 0:
+			queue_free()
--- Projectiles/wand.gd
+++ Projectiles/wand.gd
@@ -31,7 +31,24 @@ func _ready():
 	add_to_group("projectiles")
 
 
+
 func _on_body_entered(body):
 	if body.is_in_group("mob"):
-		body.queue_free()
+		emit_signal("hit", body)
 	queue_free()
+
+
+#func _on_body_entered(body):
+#	if body.name == "Player":
+#		pass
+#	else:
+#		queue_free() 
+
+
+
+
+func _on_area_entered(area):
+	queue_free() # Replace with function body.
+
+
+ # Replace with function body.
