8b2605afa300fbab42d42cfffcff46a066251465
Alice Egranov <egranovalice@gmail.com>
2023-10-30 13:50:07 -0500

    Update main.gd

--- main.gd
+++ main.gd
@@ -11,12 +11,12 @@ extends Node2D
 		#game_paused = value
 		#emit_signal("toggle_game_paused", game_paused)
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass
+func _ready():
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-##func _process(_delta):
+func _process(_delta):
 	pass
 	
 #func _input(event: InputEvent):
